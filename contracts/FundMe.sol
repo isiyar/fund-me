074f85cbf0924ceeb4bb7a5ffc5c1696ff68493d
resgjk <aremijplohih@gmail.com>
2024-12-05 19:15:29 +0500

    feat: Add fund and getVersion functions

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -7,9 +7,36 @@ import "./PriceConverter.sol";
 contract FundMe {
 	using PriceConverter for uint256;
 
+	mapping (address => uint256) public addressToAmountFunded;
+	address[] public funders;
+
+	address public owner;
+	address public oracleAddress;
+  uint256 public constant MINIMUM_USD = 50 * 1e18;
+
+	constructor(address oracleAddress_) {
+		oracleAddress = oracleAddress_;
+		owner = msg.sender;
+	}
+
+	function getVersion(address _oracleAddress) public view returns (uint256) {
+		AggregatorV3Interface dataFeed = AggregatorV3Interface(_oracleAddress);
+		return dataFeed.version();
+	}
+
 	function fund() public payable {
-		//require(msg.value >= minimumUsd, "Insufficient funds");
+		require(msg.value.getConversionRate(oracleAddress) >= MINIMUM_USD, "You need to spend more ETH!");
+		funders.push(msg.sender);
+		addressToAmountFunded[msg.sender] += msg.value;
 	}
 
 	// function withdraw() {}
+
+	fallback() external payable {
+		fund();
+	}
+
+	receive() external payable {
+		fund();
+	}
 }
